f7c71573ed2a250779f48f51e0b6f4092a235cab
Sergey Tsybenko <cybenko.sergey@gmail.com>
2023-10-21 13:32:29 -0600

    Add create post api

--- src/Canvas/Api.jsx
+++ src/Canvas/Api.jsx
@@ -1,18 +1,32 @@
 import axios from 'axios'
 
-export const uploadPost = (file, title) => {
-    axios.post('http://localhost:8000/posts/create', {file: file}, {
-        data: {
-            title: title
-        },
+export const uploadFile = (file, title, setMessage) => {
+    axios.post('http://localhost:8000/posts/upload-file', {file: file}, {
         headers: {
           "Content-Type": "multipart/form-data",
           'Authorization': 'Bearer '+ localStorage.getItem("auth-token"),
         }
       }
-    ).then(function (response) {
-        console.log(response)
-      }).catch((e) => {
-            console.log(e)
-        })
-      }
+    ).then((response) => {
+        uploadPost(title, response.data, setMessage)
+      })
+    .catch((e) => {
+        setMessage(e.message)
+    })
+}
+
+export const uploadPost = (title, filename, setMessage) => {
+    fetch('http://localhost:8000/posts/create', {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+          'Authorization': 'Bearer '+ localStorage.getItem("auth-token"),
+        },
+        body: JSON.stringify({title: title, img: filename})
+    }).then((response) => {
+        setMessage('Created!')
+      })
+    .catch((e) => {
+        setMessage(e.message)
+    })
+}
--- src/Canvas/Canvas.jsx
+++ src/Canvas/Canvas.jsx
@@ -1,65 +1,15 @@
-import React, { useState, useContext } from 'react'
+import React, { useState } from 'react'
 import DrawingCanvas from './DrawingCanvas';
-import {uploadPost} from './Api'
+import {uploadFile} from './Api'
+import { Link } from 'react-router-dom';
 
 function Canvas() {
   const [getImage, setGetImage] = useState(() => {})
   const [title, setTitle] = useState('')
-  const [urls, setUrls] = useState([
-    {id: 1, comment: "Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol ", username: "James Strawhead", time: 1697851928864, likes: 15},
-    {id: 1, comment: "Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol ", username: "James Strawhead", time: 1697851928864, likes: 15},
-    {id: 1, comment: "Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol ", username: "James Strawhead", time: 1697851928864, likes: 15},
-    {id: 1, comment: "Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol ", username: "James Strawhead", time: 1697851928864, likes: 15},
-    {id: 1, comment: "Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol Kayaking is tons of fun lol ", username: "James Strawhead", time: 1697851928864, likes: 15}
-  ]);
-  
-  const [drawing, setDrawing] = useState(
-    {
-      id: 1, 
-      title: "Kayaking is tons of fun lol", 
-      author: "James Strawhead", 
-      time: 1697851928864, 
-      data: [
-        {
-          canvas: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fktoo%2F2013%2F09%2F090513TB_Kayaks.jpg&f=1&nofb=1&ipt=dbe0ce79a68ec8d81d7df20f708c8b1ffb25b1a64cdbda312dd4b6ae57266707&ipo=images', description: ''}, {canvas: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fktoo%2F2013%2F09%2F090513TB_Kayaks.jpg&f=1&nofb=1&ipt=dbe0ce79a68ec8d81d7df20f708c8b1ffb25b1a64cdbda312dd4b6ae57266707&ipo=images', 
-          description: ''
-        }
-      ]
-    });
-  const [date, setDate] = useState(new Date());
-  const findTime = (d, time) => {
-    var currentTime = d.getTime() / 1000;
-    time = time / 1000;
-
-    var timeYear = Math.floor((currentTime - time)/60/60/24/7/52);
-    var timeMonth = Math.floor((currentTime - time)/60/60/24/30);
-    var timeWeek = Math.floor((currentTime - time)/60/60/24/7);
-    
-    var timeDay = Math.floor((currentTime - time)/60/60/24);
-    var timeHour = Math.floor((currentTime - time)/60/60);
-    var timeMin = Math.floor((currentTime - time)/60);
-    var timeSec = Math.floor((currentTime - time));
-
-    var timeString = "";
-    if (timeSec < 60) {
-        timeString = timeSec + "s";
-    } else if (timeMin < 60) {
-        timeString = timeMin + "m";
-    } else if (timeHour < 24) {
-        timeString = timeHour + "h";
-    } else if (timeDay < 7) {
-        timeString = timeDay + "d";
-    } else if (timeDay < 30) {
-        timeString = timeWeek + "w";
-    } else {
-        timeString = "incorrect time";
-    }
-    
-    return timeString;
-  };
+  const [message, setMessage] = useState('')
 
   const createPost = () => {
-    uploadPost(getImage(), title)
+    uploadFile(getImage(), title, setMessage)
   }
 
   return (
@@ -74,9 +24,11 @@ function Canvas() {
             </div>
             
             <div className='to-the-left left-view canvas-back-btn'>
-              <div className='back-button back-view'>
-                <img src="/back.png"></img>
-              </div>
+              <Link to={'..'}>
+                <div className='back-button back-view'>
+                  <img src="/back.png" alt=''/>
+                </div>
+              </Link>
             </div>
             <input 
               value={title}
@@ -89,8 +41,10 @@ function Canvas() {
 
         {/* CANVAS */}
         <div className='canvas-box'>
-          <DrawingCanvas setGetImage={setGetImage}/>
+          <DrawingCanvas
+            setGetImage={setGetImage}/>
         </div>
+        <p>{message}</p>
 
       </div>
     </>
